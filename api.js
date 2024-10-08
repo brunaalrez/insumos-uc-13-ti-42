54158e507bcefebb582edd327ab72be9b8cf59cd
Frediney <fredineycd@hotmail.com>
2024-10-07 22:09:47 -0300

    adicionei api historico

--- api.js
+++ api.js
@@ -5,6 +5,9 @@ var vusuarios = []
 const  cr_usuarios = require('./user')
 app.user('/usuarios', cr_usuarios)
 
+const cr_historico =  require('./Historico')
+app.use('/historico', cr_historico)
+
 app.listen(3000, () => {
     console.log('http://localhost:3000')
 })
