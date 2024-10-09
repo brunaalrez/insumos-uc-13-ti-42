cc27c835f12675b7f2f4e71ef6d88f8884e3f032
Gabrielbielvl <gabrielbirges5@gmail.com>
2024-10-09 20:05:56 -0300

    ultima atualização usuarios

--- api.js
+++ api.js
@@ -1,7 +1,6 @@
 const express =  require('express')
 const app = express()
 app.use( express.json())
-var vusuarios = []
 
 const  cr_usuarios = require('../routes/user')
 app.user('/usuarios', cr_usuarios)
