3572eba8f8d09ae724be59adecb21dea29f62123
Frediney <fredineycd@hotmail.com>
2024-10-09 20:58:56 -0300

    correçao_api

--- api.js
+++ api.js
@@ -8,7 +8,7 @@ app.use('/usuarios', cr_usuarios)
 const cr_historico =  require('./routes/historico')
 app.use('/historico', cr_historico)
 
-const cr_insumos = require('../routes/insumos')
+const cr_insumos = require('./routes/insumos')
 app.use('/insumos', cr_insumos)
 
 app.listen(3000, () => {
