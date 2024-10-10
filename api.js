8d1d472150591793235ac447a5069eb996a6f8f1
brunaalrez <brunaalrez@gmail.com>
2024-10-09 21:06:23 -0300

    oh

--- api.js
+++ api.js
@@ -8,8 +8,8 @@ app.use('/usuarios', cr_usuarios)
 const cr_historico =  require('./routes/historico')
 app.use('/historico', cr_historico)
 
-//const cr_insumos = require('./routes/insumos')
-//app.use('/insumos', cr_insumos)
+const cr_insumos = require('./routes/insumos')
+app.use('/insumos', cr_insumos)
 
 app.listen(3000, () => {
     console.log('http://localhost:3000')
--- routes/insumos.js
+++ routes/insumos.js
@@ -5,18 +5,14 @@ const cuser = require('../controllers/insumos')
 
 router.use(express.json());
 
-router.post('/insumos', cuser.lista_insumos)
+router.post('/create', cuser.lista_insumos)
 
-router.get('/insumos', cuser.read_insumos)
+router.get('/read', cuser.read_insumos)
 
-router.get('/insumos/:id', cuser.read_parametros)
+router.get('/show/:id', cuser.read_parametros)
 
-router.put('/insumos/:id', cuser.update_insumos)
+router.put('/update/:id', cuser.update_insumos)
 
-router.delete('/insumos/:id', cuser.delete_insumos)
+router.delete('/delete/:id', cuser.delete_insumos)
 
 module.exports = router
-
-router.listen(3000, () => {
-    console.log('http://localhost:3000')
-})
