44ad0e3cae04e927bf8a658b7a3b17b63d185095
Frediney <fredineycd@hotmail.com>
2024-10-09 21:04:43 -0300

    comentei_insumos_api

--- api.js
+++ api.js
@@ -5,11 +5,11 @@ app.use( express.json())
 const  cr_usuarios = require('./routes/user')
 app.use('/usuarios', cr_usuarios)
 
-//const cr_historico =  require('./routes/historico')
-//app.use('/historico', cr_historico)
+const cr_historico =  require('./routes/historico')
+app.use('/historico', cr_historico)
 
-const cr_insumos = require('./routes/insumos')
-app.use('/insumos', cr_insumos)
+//const cr_insumos = require('./routes/insumos')
+//app.use('/insumos', cr_insumos)
 
 app.listen(3000, () => {
     console.log('http://localhost:3000')
